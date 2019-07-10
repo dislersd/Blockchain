f7f3d81770d37282b53fff5d693e1c2886c3bcd6
dislersd <dislersd@gmail.com>
2019-07-10 16:26:46 -0700

    miner file is communicating with blockchain server

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -14,7 +14,7 @@ class Blockchain(object):
         self.current_transactions = []
         self.nodes = set()
 
-        self.new_block(previous_hash=1, proof=100)
+        self.new_block(previous_hash=1, proof=99)
 
     def new_block(self, proof, previous_hash=None):
         """
@@ -76,24 +76,10 @@ class Blockchain(object):
     def last_block(self):
         return self.chain[-1]
 
-    def proof_of_work(self, last_proof):
-        """
-        Simple Proof of Work Algorithm
-        - Find a number p' such that hash(pp') contains 4 leading
-        zeroes, where p is the previous p'
-        - p is the previous proof, and p' is the new proof
-        """
-
-        proof = 0
-        while self.valid_proof(last_proof, proof) is False:
-            proof += 1
-
-        return proof
-
     @staticmethod
     def valid_proof(last_proof, proof):
         """
-        Validates the Proof:  Does hash(last_proof, proof) contain 4
+        Validates the Proof:  Does hash(last_proof, proof) contain 6
         leading zeroes?
         """
         guess = f'{last_proof}{proof}'.encode()
@@ -140,33 +126,38 @@ node_identifier = str(uuid4()).replace('-', '')
 blockchain = Blockchain()
 
 
-@app.route('/mine', methods=['GET'])
+@app.route('/mine', methods=['POST'])
 def mine():
-    # We run the proof of work algorithm to get the next proof...
-    last_block = blockchain.last_block
-    last_proof = last_block['proof']
-    proof = blockchain.proof_of_work(last_proof)
+    value = request.get_json()
 
-    # We must receive a reward for finding the proof.
-    # The sender is "0" to signify that this node has mine a new coin
-    blockchain.new_transaction(
-        sender="0",
-        recipient=node_identifier,
-        amount=1,
-    )
-
-    # Forge the new BLock by adding it to the chain
-    previous_hash = blockchain.hash(last_block)
-    block = blockchain.new_block(proof, previous_hash)
+    valid_proof(value)
 
     response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
+        "res": 'It works!'
     }
+
     return jsonify(response), 200
+
+    # # We must receive a reward for finding the proof.
+    # # The sender is "0" to signify that this node has mine a new coin
+    # blockchain.new_transaction(
+    #     sender="0",
+    #     recipient=node_identifier,
+    #     amount=1,
+    # )
+
+    # # Forge the new BLock by adding it to the chain
+    # previous_hash = blockchain.hash(last_block)
+    # block = blockchain.new_block(proof, previous_hash)
+
+    # response = {
+    #     'message': "New Block Forged",
+    #     'index': block['index'],
+    #     'transactions': block['transactions'],
+    #     'proof': block['proof'],
+    #     'previous_hash': block['previous_hash'],
+    # }
+    # return jsonify(response), 200
 
 
 @app.route('/transactions/new', methods=['POST'])
@@ -196,9 +187,18 @@ def full_chain():
     return jsonify(response), 200
 
 
+@app.route('/last_proof', methods=['GET'])
+def last_proof():
+    response = {
+        'last_proof': blockchain.last_block['proof']
+    }
+
+    return jsonify(response), 200
+
 # Note, when demoing, start with this, then change to the below
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=5000)
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -4,7 +4,29 @@ import requests
 import sys
 
 
-# TODO: Implement functionality to search for a proof 
+# TODO: Implement functionality to search for a proof
+def proof_of_work(last_proof):
+        """
+        Simple Proof of Work Algorithm
+        - Find a number p' such that hash(pp') contains 4 leading
+        zeroes, where p is the previous p'
+        - p is the previous proof, and p' is the new proof
+        """
+
+        proof = 0
+        while valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+def valid_proof(last_proof, proof):
+    """
+    Validates the Proof:  Does hash(last_proof, proof) contain 4
+    leading zeroes?
+    """
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:4] == "0000"
 
 
 if __name__ == '__main__':
@@ -18,8 +40,13 @@ if __name__ == '__main__':
     # Run forever until interrupted
     while True:
         # TODO: Get the last proof from the server and look for a new one
+        res = requests.get('http://localhost:5000/last_proof')
+        res = res.json()
+        new_proof = proof_of_work(res['last_proof'])
+        print(new_proof)
         # TODO: When found, POST it to the server {"proof": new_proof}
+        response = requests.post('http://localhost:5000/mine', json={'proof': new_proof})
         # TODO: If the server responds with 'New Block Forged'
+        
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
